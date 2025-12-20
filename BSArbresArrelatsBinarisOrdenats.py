f0c428d84e469edd833bfb3b97c229baae925241
JaumeVicens <jaumevicensferrer9@gmail.com>
2025-12-20 14:41:52 +0100

    Add files via upload

---
+++ BSArbresArrelatsBinarisOrdenats.py
@@ -0,0 +1,18 @@
+import math
+#import random
+import numpy as np
+
+def B(x):
+    return (1-math.sqrt(1-4*x**2))/(2*x)
+
+# Boltmzann Sampler per a la classe dels arbres arrelats binaris ordenats amb mida donada
+# pel nombre total de nodes
+
+def BSArbresArrelatsBinarisOrdenats(x):
+    u = np.random.uniform(0,1)
+    if u <= (x/B(x)):
+        return '*'
+    else:
+        return ('*',BSArbresArrelatsBinarisOrdenats(x),BSArbresArrelatsBinarisOrdenats(x))
+
+#print(BSArbresArrelatsBinarisOrdenats(0.499))
