bb985148e716d7b013f29127875996bd24a97091
JaumeVicens <jaumevicensferrer9@gmail.com>
2025-12-20 14:22:46 +0100

    Update BSSeqüència.py

--- BSSeqüència.py
+++ BSSeqüència.py
@@ -4,19 +4,16 @@ import numpy as np
 
 
 # Boltzmann Sampler per a la classe combinatòria Seq(A), on A es una classe combinatòria
-# finita qualsevol. 'b' representa l'atom de mida 0
+# finita qualsevol. '' representa l'atom de mida 0 (paraula buida)
 
 def BoltmannSamplerSequencia(A,x):
     u = np.random.uniform(0,1)
-    #print(u)
-    #print(generatriu_avaluada(A,x))
     if u <= generatriu_avaluada(A,x):
         return (BoltmzannSamplerFinit(A,x),BoltmannSamplerSequencia(A,x))
 
     else:
-        return 'b'
+        return ''
 
 A = {'0': 1, '1': 1}
 
-print(BoltmannSamplerSequencia(A,0.45))  #esta definida per x < 1/2 ja que s'ha de complir
-                                            # que 1 > A(x) = 2x
+print(BoltmannSamplerSequencia(A,0.45))  #esta definida per x < 1/2 ja que s'ha de complir que 1 > A(x) = 2x
